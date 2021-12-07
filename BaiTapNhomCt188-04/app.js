0988e697d95fced3921f63e8b829f6d7589e9bbc
ThaiPham <95671921+ThaiPhamGroup4@users.noreply.github.com>
2021-12-07 15:25:16 +0700

    Upload html  95%

--- BaiTapNhomCt188-04/app.js
+++ BaiTapNhomCt188-04/app.js
@@ -2,7 +2,7 @@
 let toTopBtn = document.querySelector('.toTop')
 console.log(toTopBtn)
 window.onscroll = () => {
-    if(this.scrollY < 140) {
+    if(this.scrollY < 200) {
         toTopBtn.style.opacity = 0;
     } else {
         toTopBtn.style.opacity = 1;
@@ -12,3 +12,21 @@ toTopBtn.addEventListener('click', () => {
     // document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0
 })
+
+// Đồng hồ 
+function clock() {
+    const time = new Date;
+    const h = time.getHours();
+    let m = time.getMinutes();
+    let timeHtml = document.querySelector('body .clock');
+    let s = time.getSeconds();
+    // validate time
+    if(s < 10) { 
+        s = '0' + s}
+    else if(m < 10) {
+        m = '0' + m;
+    }
+    timeHtml.innerText = `${h}:${m}:${s}`;
+    setTimeout(clock, 1000)
+}
+clock()
