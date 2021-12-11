0a4352ae127097dd8f222e07de8211e4cffe7992
ThaiPham <95671921+ThaiPhamGroup4@users.noreply.github.com>
2021-12-11 22:02:50 +0700

    99% trang giohang

--- BaiTapNhomCt188-04/giohang.js
+++ BaiTapNhomCt188-04/giohang.js
@@ -13,11 +13,10 @@ renderProduct()
 function renderProduct() {
     let table = document.querySelector('.product__table')
     // Nếu giỏ hàng trống 
-    if(arrCart.length === 0) {
-        
-        table.parentNode.style.height ="auto"
-        table.parentNode.style.minHeight ="50vh"
+    if (arrCart.length === 0) {
 
+        // table.parentNode.style.height ="auto"
+        // table.parentNode.style.minHeight ="50vh"
         table.innerHTML += `
             <tr>
                 <td colspan="6"><p>Bạn chưa có sản phẩm nào trong giỏ, mua sắm ngay tại <a style="color: blue;" href="./sanpham.html">Đây</a></p></td>
@@ -43,7 +42,7 @@ function renderProduct() {
                 </tr>
             `
         })
-        table.innerHTML += newTableHtml
+        table.innerHTML += newTableHtml.join(' ')
     }
 }
 // quantilyNumber lưu tổng sản phẩm
@@ -56,17 +55,14 @@ function getAllQuantily() {
     return quantilyNumber
 }
 
-// Hàm tính tổng tiền
-function totalMoney() {
 
-}
 // Hàm xoá sản phẩm sau khi ấn nút xoá
 deleteProduct()
 function deleteProduct() {
     let deleteBtn = document.querySelectorAll('.deleteBtn')
     // console.log(deleteBtn)
-    deleteBtn.forEach((item)=> {
-        item.onclick = function(e) {
+    deleteBtn.forEach((item) => {
+        item.onclick = function (e) {
             // Lấy id của item cần xoá
             let idDelete = e.target.parentNode.parentNode.children[0].innerText
             deleteInLocalStorage(idDelete)
@@ -85,19 +81,18 @@ function deleteInLocalStorage(idDelete) {
 }
 
 // /////////////////// Làm chức năng chỉnh sửa số lượng sản phẩm trong giỏ hàng
-let inputQuantily = document.querySelectorAll('.table__product-quantily') 
+let inputQuantily = document.querySelectorAll('.table__product-quantily')
 let btnConfirm = document.querySelector('.confirm')
 // Duyet qua cac co input để thêm sự kiện onchange
 inputQuantily.forEach((item) => {
-    item.onchange = function(e) {
+    item.onchange = function (e) {
         // Lưu giá trị quatily mới
         let newValue = e.target.value
         //Biến lưu id của sản phẩm chỉnh sửa
         let idModify = e.target.parentNode.parentNode.children[0].innerText
-        console.log(idModify)
-         //Sửa lại quantily trong mảng chứa sp trong giỏ   
+        //Sửa lại quantily trong mảng chứa sp trong giỏ   
         arrCart.map((item) => {
-            if(item.id === idModify) {
+            if (item.id === idModify) {
                 item.quantily = Number(newValue)
             }
         })
@@ -106,22 +101,77 @@ inputQuantily.forEach((item) => {
     }
 })
 // Khi focus vào ô input số lượng sản phẩm thì hiện nút xác nhận
-inputQuantily.forEach((item) => {
-    item.onfocus = function () {
-        this.nextElementSibling.style.display = 'inline-block'
-    }
-})
+// inputQuantily.forEach((item) => {
+//     item.onfocus = function () {
+//         // this.nextElementSibling.style.display = 'inline-block'
+//     }
+// })
 
 // cập nhật lại số lượng sản phẩm trong Storage
 function updateQuantily() {
     btnConfirm.onclick = (e) => {
-            // ẩn nút xác nhận
-            // Thông báo thành công
-            alert('Chỉnh sửa thành công')  
-            localStorage.setItem('Cart', JSON.stringify(arrCart))
-            // reload lại trang 1 cái để render lại sản phẩm
-            window.location.reload()
+        // ẩn nút xác nhận
+        // Thông báo thành công
+        alert('Chỉnh sửa thành công')
+        localStorage.setItem('Cart', JSON.stringify(arrCart))
+        // reload lại trang 1 cái để render lại sản phẩm
+        window.location.reload()
     }
-   
+
 }
 
+// Phần tính toán tiền
+renderToPaymentTable()
+function renderToPaymentTable () {
+    // Lấy danh sách các tên sản phẩm và số lượng từng sản phẩm đưa vào bảng
+    getListProduct(arrCart)
+
+    totalMoney()
+    // Tính tổng tiền
+
+}
+
+// Hàm tính tổng tiền
+function totalMoney() {
+    let money = 0
+    arrCart.forEach((item) => {
+        money += Number(item.price * item.quantily)
+      });
+      return money
+}
+// Lấy danh sách các tên sản phẩm và số lượng từng sản phẩm đưa vào bảng
+
+function getListProduct(arrCart) {
+    let total = totalMoney()
+        // tính tổng tiền
+    let htmlTablePayment = document.querySelector('.total__wrapper table')
+    let htmlNew = arrCart.map((item) => {
+        let nameProduct = item.name
+        let quantily = item.quantily
+        let priceTotal = item.quantily * item.price
+        console.log(htmlTablePayment)
+        console.log(nameProduct)
+        console.log(quantily)
+        console.log(priceTotal)
+        console.log(total)
+
+        // Thêm html vào bảng
+        return `
+            <tr>
+                <td>${nameProduct}</td>
+                <td>${quantily}</td>
+                <td>${priceTotal}</td>
+            </tr>
+
+        `
+    })
+    htmlTablePayment.innerHTML += htmlNew.join('')
+    htmlTablePayment.innerHTML += `
+        <tr>
+            <th colspan="2">Thành tiền</th>
+            <td>${total}</td>
+        </tr>
+    `
+    
+    
+}
