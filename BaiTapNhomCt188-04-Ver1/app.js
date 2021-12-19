3e36a2c1984ab2967c0b65e47abe45f5dcae4e1a
ThaiPham <95671921+ThaiPhamGroup4@users.noreply.github.com>
2021-12-19 18:01:39 +0700

    update ver 1 and rename

---
+++ BaiTapNhomCt188-04-Ver1/app.js
@@ -0,0 +1,189 @@
+/* By Phạm Quang Thái B2012257 */
+// Js dùng cho trang chủ, san pham và trang chi tiết sp
+
+// Nút ToTop
+let toTopBtn = document.querySelector('.toTop')
+window.onscroll = () => {
+    if (this.scrollY < 160) {
+        toTopBtn.style.opacity = 0;
+    } else {
+        toTopBtn.style.opacity = 1;
+    }
+}
+toTopBtn.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0
+})
+
+
+// Đồng hồ 
+function clock() {
+    const time = new Date;
+    const h = time.getHours();
+    let m = time.getMinutes();
+    let timeHtml = document.querySelector('body .clock');
+    let s = time.getSeconds();
+    // validate time
+    if (s < 10) {
+        s = '0' + s;
+    }
+    if (m < 10) {
+        m = '0' + m;
+    }
+    timeHtml.innerText = `${h}:${m}:${s}`;
+    setTimeout(clock, 1000)
+}
+clock()
+
+/////////////// Hàm Hiện thông báo khi thêm thành công ///////////////////////
+// body là thẻ html body
+let body = document.querySelector('#container')
+function showSuccessMessage() {
+    // Tao 1 element co class toast
+    const htmlToast = document.createElement("div");
+    htmlToast.classList.add('toast')
+    htmlToast.innerHTML = `
+            <div class="toast__icon">
+                <i class="fas fa-check-circle"></i>
+            </div>
+
+            <div class="toast__body">
+                <h3 class="toast__title">Success</h3>
+                <p class="toast__msg">Thêm thành công vào giỏ hàng</p>
+            </div>
+            <div class="toast__close-icon">
+            <i class="fas fa-times" title="Đóng"></i>
+        </div>
+           
+    `
+    // Thêm toast vào body
+    body.append(htmlToast)
+    // Remove Message sau 3 giây
+    setTimeout(() => {
+        body.removeChild(htmlToast)
+    }, 3000)
+
+    // Đóng toast khi click
+    removeToastAfterClick(htmlToast)
+}
+
+// remove toast sau khi click nút close
+function removeToastAfterClick(htmlToast) {
+    let clockBtn = document.querySelectorAll('.toast__close-icon')
+        clockBtn.forEach((item) => {
+        item.addEventListener('click', () => {
+            body.removeChild(htmlToast)
+        })
+    })
+}
+
+
+
+// ////////////////////chức năng thêm vào giỏ hàng dùng chung cho trang chủ, và trang chi tiết sản phẩm//////////////////////
+
+// arrCart là mảng chứa các sản phẩm trong localStorage
+let arrCart = JSON.parse(localStorage.getItem('Cart'));
+// Khi tải trang sẽ hiển thị lại số tượng sản phẩm trong giỏ
+window.onload = function () {
+    showQuantily(arrCart)
+}
+// Kiểm tra có hổ trợ LocalStorage hay không
+if (typeof (Storage) !== 'undefined') {
+    // Khởi tạo cart trong localStorage
+    setInStorage();
+    // thêm sự kiện click vào tất cả nút Mua
+    let buyBtns = document.querySelectorAll('.info__price-button')
+    buyBtns.forEach((buyBtn) => {
+        buyBtn.addEventListener('click', (e) => {
+            // let idProduct = e.target.parentNode.children[3].innerText
+            let idProduct = e.target.parentNode.querySelector('.id').innerText
+            // let nameProduct = e.target.parentNode.children[0].innerText
+            let nameProduct = e.target.parentNode.querySelector('.info__title').innerText
+            // let priceProduct = e.target.parentNode.children[2].innerText
+            let priceProduct = e.target.parentNode.querySelector('.price').innerText
+            // let imgSrc = e.target.parentNode.previousElementSibling.attributes[0].nodeValue;
+            let imgSrc = e.target.parentNode.parentNode.querySelector('.item__image').attributes[0].nodeValue
+
+            // Đưa dữ liệu lấy được vào object
+            let newProducts = {
+                id: idProduct,
+                name: nameProduct,
+                price: priceProduct,
+                src: imgSrc,
+                quantily: 1,
+            }
+            // đưa newProducts vào LocalStorge
+            addInStorage(newProducts)
+        })
+    })
+
+
+} else {
+    //Nếu không hỗ trợ
+    alert('Trình duyệt của bạn không hỗ trợ Storage');
+}
+
+// kiểm tra nếu chưa có key Cart trong Store thì thêm value Cart vào
+// cart là mảng chứa sản phẩm đã đc khách hàng click thêm vào giỏ 
+function setInStorage() {
+    let cart = new Array;
+    if (localStorage.key('Cart') == 'Cart') {
+        // Đã có key trong Storage
+    }
+    else {
+
+        localStorage.setItem('Cart', JSON.stringify(cart))
+    }
+}
+
+function addInStorage(newProducts) {
+    // id gán bằng id của sản phẩm sắp đc thêm
+    let id = newProducts.id;
+    // Kiểm tra nếu mảng arrCart này rổng thì thêm vô luôn, ngược lại thì xét 2 trường hợp
+    if (arrCart.length === 0) {
+        arrCart.push(newProducts)
+    }
+    else {
+        let isExis = isInLocalStorage(id, arrCart)
+        // trường hợp 1 nếu isExis bằng -10 thì tương đương k tồn tại sp trùng => thêm vào mảng luôn
+        if (isExis === -10) {
+            arrCart.push(newProducts)
+        }
+        // trường hợp 2 isExis khắc -10 => tồn tại sp trong giỏ rồi => cho thuộc tính quatity(số lượng) của sp đó tăng lên 1
+        else arrCart[isExis].quantily++
+    }
+    // Đưa arrCart đã xử lý vào LocalStorage
+    localStorage.setItem('Cart', JSON.stringify(arrCart))
+    // Thông báo thêm thành công
+    addProductSuccess()
+    // Hiển thị lại số sản phẩm trong giỏ hàng ngay khi thêm thành công mà k reload lại trang
+    showQuantily(arrCart)
+}
+// hàm kiểm tra có tồn tại sản phẩm này trong Storage chưa
+//  indexNew === -10 là không tồn tại trong arrCart và nếu !== -10 thì có tồn tại
+function isInLocalStorage(id, arrCart) {
+    let indexNew = -10;
+    arrCart.forEach((item, index) => {
+        if (id === item.id) indexNew = index;
+    })
+    return indexNew
+}
+
+// Hiện 1 thông báo khi thêm thành công
+function addProductSuccess() {
+    // alert("Thông báo thêm sản phẩm thành công!")
+    // Thong bao thêm thành công
+    showSuccessMessage()
+}
+// Hiển thị số sp trong giỏ
+function showQuantily() {
+    let quantily = 0
+    let cartNumber = document.querySelector('.cart-number')
+    // Duyệt qua mảng arrCart lấy từng trường quantily của 1 sản phẩm cộng vào biến quantily
+    arrCart.map((item) => {
+        quantily += item.quantily;
+    })
+    cartNumber.innerText = quantily;
+}
+
+
+
---
+++ BaiTapNhomCt188-04-Ver1/giohang.js
@@ -0,0 +1,165 @@
+// ////////////Phần Javascript của trang giỏ hàng/////////////////
+// Javascript dùng cho trang giỏ hàng: render ra list sản phẩm đã thêm, 
+// Thành tiền (Xem như đã đăng nhập thành công rồi => k yêu cầu đăng nhập), xoá, chỉnh sửa số lượng sp
+
+/* Sau khi xoá 1 sản phẩm xong thì nếu có bật 1 tab trangchu.html or sanpham.html thì phải reload lại trang, 
+nếu k sẽ bị lỗi
+Do số lượng sản phẩm trong giỏ của 2 trang 'trangchu.html' và 'sanpham.html' chưa được cập nhật lại
+*/
+/* By Phạm Quang Thái B2012257 */
+
+
+// render list sản phẩm đã thêm ra html và thêm vào bảng 
+renderProduct()
+function renderProduct() {
+    let table = document.querySelector('.product__table')
+    // Nếu giỏ hàng trống 
+    if (arrCart.length === 0) {
+
+        // table.parentNode.style.height ="auto"
+        // table.parentNode.style.minHeight ="50vh"
+        table.innerHTML += `
+            <tr>
+                <td colspan="6"><p>Bạn chưa có sản phẩm nào trong giỏ, mua sắm ngay tại <a style="color: blue;" href="./sanpham.html">Đây</a></p></td>
+            </tr>
+        `
+    }
+    // Ngược lại, => giỏ hàng có ít nhất 1 sản phẩm
+    else {
+        newTableHtml = arrCart.map((item) => {
+            return `
+                <tr>
+                    <td hidden>${item.id}</td>
+                    <td><img class="table__product-img" src="${item.src}" alt="img" ></td>
+                    <td><a href="./chitiet.html" class="table__product-name">${item.name}</a></td>
+                    <td>
+                        <input type="number" value="${item.quantily}" min="1" name="inputQuantily" class="table__product-quantily" />
+                        
+                    </td>
+                    <!-- <td><p class="table__product-quantily">${item.quantily}</p></td> -->
+                    <td><p class="table__product-price">${item.price}</p></td>
+                    <td></td>
+                    <td colspan='1' class="table__modify"><button class="deleteBtn">Xoá mục này</button> </td>
+                </tr>
+            `
+        })
+        table.innerHTML += newTableHtml.join(' ')
+    }
+}
+// quantilyNumber lưu tổng sản phẩm
+var quantilyNumber = getAllQuantily()
+function getAllQuantily() {
+    let quantilyNumber = 0;
+    arrCart.map((item) => {
+        quantilyNumber += item.quantily
+    })
+    return quantilyNumber
+}
+
+
+// Hàm xoá sản phẩm sau khi ấn nút xoá
+deleteProduct()
+function deleteProduct() {
+    let deleteBtn = document.querySelectorAll('.deleteBtn')
+    // console.log(deleteBtn)
+    deleteBtn.forEach((item) => {
+        item.onclick = function (e) {
+            // Lấy id của item cần xoá
+            let idDelete = e.target.parentNode.parentNode.children[0].innerText
+            deleteInLocalStorage(idDelete)
+        }
+    })
+}
+// Xoá sản phẩm có id là idDelete trong localStorage
+function deleteInLocalStorage(idDelete) {
+    let arrAfterDelete = arrCart.filter((item) => {
+        return item.id !== idDelete;
+    })
+    arrCart = arrAfterDelete;
+    // Cập nhật lại mảng các sản phầm trong giỏ vào LocalStorage
+    localStorage.setItem('Cart', JSON.stringify(arrCart))
+    window.location.reload()
+}
+
+// /////////////////// Làm chức năng chỉnh sửa số lượng sản phẩm trong giỏ hàng
+let inputQuantily = document.querySelectorAll('.table__product-quantily')
+let btnConfirm = document.querySelector('.confirm')
+// Duyet qua cac co input để thêm sự kiện onchange
+inputQuantily.forEach((item) => {
+    item.onchange = function (e) {
+        // Lưu giá trị quatily mới
+        let newValue = e.target.value
+        //Biến lưu id của sản phẩm chỉnh sửa
+        let idModify = e.target.parentNode.parentNode.children[0].innerText
+        //Sửa lại quantily trong mảng chứa sp trong giỏ   
+        arrCart.forEach((item) => {
+            if (item.id === idModify) {
+                item.quantily = Number(newValue)
+            }
+        })
+        // Gọi hàm này để thêm arrCart mới(sau khi chinh sửa) vào LocalStorage
+        updateQuantily()
+    }
+})
+
+// cập nhật lại số lượng sản phẩm trong Storage
+function updateQuantily() {
+    btnConfirm.onclick = (e) => {
+        // Thông báo thành công
+        alert('Chỉnh sửa thành công')
+        // showSuccessMessage()
+        localStorage.setItem('Cart', JSON.stringify(arrCart))
+        // reload lại trang 1 cái để render lại sản phẩm
+        window.location.reload()
+    }
+
+}
+
+// Phần tính toán tiền
+renderToPaymentTable()
+function renderToPaymentTable () {
+    // Lấy danh sách các tên sản phẩm và số lượng từng sản phẩm đưa vào bảng
+    getListProduct(arrCart)
+
+    totalMoney()
+    // Tính tổng tiền
+
+}
+
+// Hàm tính tổng tiền
+function totalMoney() {
+    let money = 0
+    arrCart.forEach((item) => {
+        money += Number(item.price * item.quantily)
+      });
+    return money
+}
+// Lấy danh sách các tên sản phẩm và số lượng từng sản phẩm đưa vào bảng
+
+function getListProduct(arrCart) {
+    // tính tổng tiền
+    let total = totalMoney()
+    let htmlTablePayment = document.querySelector('.total__wrapper table')
+    let htmlNew = arrCart.map((item) => {
+        let nameProduct = item.name
+        let quantily = item.quantily
+        let priceTotal = item.quantily * item.price
+        // Thêm html vào bảng
+        return `
+            <tr>
+                <td>${nameProduct}</td>
+                <td>${quantily}</td>
+                <td>${priceTotal}</td>
+            </tr>
+        `
+    })
+    htmlTablePayment.innerHTML += htmlNew.join(' ')
+    htmlTablePayment.innerHTML += `
+        <tr>
+            <th colspan="2">Thành tiền</th>
+            <td>${total}&#8363;</td>
+        </tr>
+    `
+    
+    
+}
